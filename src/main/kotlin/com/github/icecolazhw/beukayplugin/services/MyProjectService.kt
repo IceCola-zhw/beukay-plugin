db76bc1d8354a588e6ba6a9d1cfa8d283a00807b
GitHub Action <action@github.com>
2023-10-31 09:45:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/icecolazhw/beukayplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/icecolazhw/beukayplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.icecolazhw.beukayplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.icecolazhw.beukayplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
